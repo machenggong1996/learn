231699089b3a69b67dcf2bed9469d60f08bab13f
machenggong <machenggong@beyondsoft.com>
2019-11-25 21:16:16 +0800

    inttoroman

--- src/MaxK.java
+++ src/MaxK.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 /**
@@ -33,19 +34,30 @@ public class MaxK {
     }
 
     public static void main(String[] args) {
-        int num, k;
-        System.out.println("请输入数字num和要分成的段数k: ");
-        while (cin.hasNext()) {
-            num = cin.nextInt();
-            k = cin.nextInt();
-            ans = Long.MIN_VALUE;
-            work(num, 0, k, 1L);
-            if (ans == Long.MIN_VALUE) {
-                System.out.println("整数" + num + "不能被分成" + k + "段");
-            } else {
-                System.out.println(num + "的最大" + k + "乘积是: " + ans);
-            }
-            System.out.println("请输入数字num和要分成的段数k: ");
-        }
+//        int num, k;
+//        System.out.println("请输入数字num和要分成的段数k: ");
+//        while (cin.hasNext()) {
+//            num = cin.nextInt();
+//            k = cin.nextInt();
+//            ans = Long.MIN_VALUE;
+//            work(num, 0, k, 1L);
+//            if (ans == Long.MIN_VALUE) {
+//                System.out.println("整数" + num + "不能被分成" + k + "段");
+//            } else {
+//                System.out.println(num + "的最大" + k + "乘积是: " + ans);
+//            }
+//            System.out.println("请输入数字num和要分成的段数k: ");
+//        }
+        Double d3 = Double.valueOf(1.0/3);
+        Double d4 = Double.valueOf(2.0/3);
+        BigDecimal d5 = new BigDecimal(1);
+        BigDecimal d6 = new BigDecimal(2);
+        BigDecimal d7 = new BigDecimal(3);
+        BigDecimal d2 = new BigDecimal(d3);
+        BigDecimal d1 = new BigDecimal(d4);
+        BigDecimal d8 = d5.divide(d7);
+        BigDecimal d9 = d6.divide(d7);
+        System.out.println();
+
     }
 }
