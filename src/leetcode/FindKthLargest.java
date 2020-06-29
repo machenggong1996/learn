13881c1866e56ecf8b8ada0859d55fe258a0bd34
machenggong1996 <mcg1996@163.com>
2020-06-29 09:35:29 +0800

    数组中的第K个最大元素

---
+++ src/leetcode/FindKthLargest.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+/**
+ * @author machenggong (chenggong@shanshu.ai)
+ * @date 2020/06/29
+ */
+public class FindKthLargest {
+
+    /**
+     * 数组中的第K个最大元素
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findKthLargest(int[] nums, int k) {
+        int heapSize = nums.length;
+        buildMaxHeap(nums, heapSize);
+        //删除堆顶4次
+        for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
+            swap(nums, 0, i);
+            --heapSize;
+            maxHeapify(nums, 0, heapSize);
+        }
+        return nums[0];
+    }
+
+    /**
+     * 构建大顶堆
+     * @param a
+     * @param heapSize
+     */
+    public void buildMaxHeap(int[] a, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; --i) {
+            maxHeapify(a, i, heapSize);
+        }
+    }
+
+    public void maxHeapify(int[] a, int i, int heapSize) {
+        int l = i * 2 + 1, r = i * 2 + 2, largest = i;
+        if (l < heapSize && a[l] > a[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && a[r] > a[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(a, i, largest);
+            maxHeapify(a, largest, heapSize);
+        }
+    }
+
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        FindKthLargest findKthLargest = new FindKthLargest();
+        System.out.println(findKthLargest.findKthLargest(new int[] { 3, 2, 1, 5, 6, 4 }, 2));
+    }
+
+}
