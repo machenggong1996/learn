10ae2f448b03c376f727f7ca53ec064c6a58b448
machenggong1996 <mcg1996@163.com>
2020-08-24 09:32:11 +0800

    重复子字符串

---
+++ src/leetcode/string/RepeatedSubstringPattern.java
@@ -0,0 +1,38 @@
+package leetcode.string;
+
+/**
+ * @author machenggong (chenggong@shanshu.ai)
+ * @date 2020/08/24
+ */
+public class RepeatedSubstringPattern {
+
+    /**
+     * 重复的子字符串
+     *
+     * @param s
+     * @return
+     */
+    public static boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i * 2 <= n; ++i) {
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; ++j) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(repeatedSubstringPattern("abcabcabc"));
+    }
+
+}
