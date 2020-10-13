41121954978c7d76df1f428f6e3484a33bb57674
machenggong1996 <mcg1996@163.com>
2020-10-13 09:26:02 +0800

    两两交换链表中的节点

---
+++ src/leetcode/list/SwapPairs.java
@@ -0,0 +1,44 @@
+package leetcode.list;
+
+/**
+ * @author machenggong
+ * @date 2020/10/13
+ */
+public class SwapPairs {
+
+    /**
+     * 24. 两两交换链表中的节点
+     * @param head
+     * @return
+     */
+
+    public static ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        ListNode res = swapPairs(listNode1);
+
+    }
+
+}
