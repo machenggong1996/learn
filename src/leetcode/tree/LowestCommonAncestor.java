c76309139a8a30f66863e3d39e312c1164986988
machenggong1996 <mcg1996@163.com>
2020-11-03 16:52:39 +0800

    二叉树的最近公共祖先

---
+++ src/leetcode/tree/LowestCommonAncestor.java
@@ -0,0 +1,58 @@
+package leetcode.tree;
+
+/**
+ * @author machenggong
+ * @date 2020/11/03
+ */
+public class LowestCommonAncestor {
+
+    /**
+     * 二叉树的最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(4);
+
+        root.left = node5;
+        root.right = node1;
+
+        node5.left = node6;
+        node5.right = node2;
+
+        node2.left = node7;
+        node2.right = node4;
+
+        node1.left = node0;
+        node1.right = node8;
+
+        TreeNode res = lowestCommonAncestor(root,node6,node7);
+        System.out.println(res.val);
+    }
+
+}
