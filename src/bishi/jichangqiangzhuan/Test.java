5e2bcca622c2badfd44ce84fb1773fb6d943d21b
machenggong1996 <mcg1996@163.com>
2020-11-05 17:00:30 +0800

    增加类型转换注释

--- src/bishi/jichangqiangzhuan/Test.java
+++ src/bishi/jichangqiangzhuan/Test.java
@@ -6,6 +6,11 @@ package bishi.jichangqiangzhuan;
  */
 public class Test {
 
+    /**
+     * 类型转化必须是有继承关系的 不然编译报错
+     * 如果引用的类型与要转换的类型不一样 运行报错
+     * @param args
+     */
     public static void main(String[] args) {
         Anamial anamial = new Lion();
         Brid brid = new Brid();
