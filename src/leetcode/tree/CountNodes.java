7d0907a5842f86987decaab8f2c368e9b442fd35
machenggong1996 <mcg1996@163.com>
2020-11-24 09:25:38 +0800

    完全二叉树的节点个数

---
+++ src/leetcode/tree/CountNodes.java
@@ -0,0 +1,78 @@
+package leetcode.tree;
+
+/**
+ * @author machenggong
+ * @date 2020/11/24
+ */
+public class CountNodes {
+
+    /**
+     * 完全二叉树的节点个数
+     *
+     * @param root
+     * @return
+     */
+    public static int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+
+        //1 为左右节点加当前节点
+        return left + right + 1;
+
+    }
+
+    public static int countNodes1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public static boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        System.out.println(countNodes1(root));
+    }
+
+}
