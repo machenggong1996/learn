0ce67b16f3710ede8ba410d88284a9c9053db88d
machenggong1996 <mcg1996@163.com>
2020-12-04 09:18:18 +0800

    分隔连续子序列

---
+++ src/leetcode/array/IsPossible.java
@@ -0,0 +1,50 @@
+package leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * @author machenggong
+ * @date 2020/12/04
+ */
+public class IsPossible {
+
+    /**
+     * 分隔连续子序列
+     *
+     * @param nums
+     * @return
+     */
+    public static boolean isPossible(int[] nums) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<Integer, PriorityQueue<Integer>>();
+        for (int x : nums) {
+            if (!map.containsKey(x)) {
+                map.put(x, new PriorityQueue<Integer>());
+            }
+            if (map.containsKey(x - 1)) {
+                int prevLength = map.get(x - 1).poll();
+                if (map.get(x - 1).isEmpty()) {
+                    map.remove(x - 1);
+                }
+                map.get(x).offer(prevLength + 1);
+            } else {
+                map.get(x).offer(1);
+            }
+        }
+        Set<Map.Entry<Integer, PriorityQueue<Integer>>> entrySet = map.entrySet();
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : entrySet) {
+            PriorityQueue<Integer> queue = entry.getValue();
+            if (queue.peek() < 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPossible(new int[] { 1, 2, 3, 3, 4, 5 }));
+    }
+
+}
