3d885707ee7f3cec499fb82864a1208c9f0e05b0
machenggong1996 <mcg1996@163.com>
2020-12-14 09:25:24 +0800

    字母异位词分组

---
+++ src/leetcode/string/GroupAnagrams.java
@@ -0,0 +1,68 @@
+package leetcode.string;
+
+import java.util.*;
+
+/**
+ * @author machenggong
+ * @date 2020/12/14
+ * @description
+ */
+public class GroupAnagrams {
+
+    /**
+     * 字母异位词分组
+     * 排序
+     *
+     * @param strs
+     * @return
+     */
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    /**
+     * 计数
+     *
+     * @param strs
+     * @return
+     */
+    public static List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            int[] counts = new int[26];
+            int length = str.length();
+            for (int i = 0; i < length; i++) {
+                counts[str.charAt(i) - 'a']++;
+            }
+            // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strArr = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams1(strArr));
+    }
+
+
+}
