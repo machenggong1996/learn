1243f3fa605637e712fe812edc465b356d290a91
machenggong1996 <mcg1996@163.com>
2020-12-17 09:45:09 +0800

    买卖股票的最佳时机含手续费

---
+++ src/leetcode/dp/MaxProfit.java
@@ -0,0 +1,57 @@
+package leetcode.dp;
+
+/**
+ * @author machenggong
+ * @date 2020/12/17
+ * @description
+ */
+public class MaxProfit {
+
+    /**
+     * 买卖股票的最佳时机含手续费
+     * 动态规划演进版
+     * 状态转移
+     *
+     * @param prices
+     * @param fee
+     * @return
+     */
+    public static int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        //sell卖出最大利润 buy买入之后手中持有一张股票最大利润
+        int sell = 0, buy = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            sell = Math.max(sell, buy + prices[i] - fee);
+            buy = Math.max(buy, sell - prices[i]);
+        }
+        return sell;
+    }
+
+    /**
+     * 动态规划
+     *
+     * @param prices
+     * @param fee
+     * @return
+     */
+    public static int maxProfitDp(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            //dp[i][0] 表示第 i 天交易完后手里没有股票的最大利润，
+            //dp[i][1] 表示第 i 天交易完后手里持有一支股票的最大利润（i 从 0 开始）
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        //第 i 天交易完后手里没有股票的最大利润
+        return dp[n - 1][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+    }
+
+
+}
