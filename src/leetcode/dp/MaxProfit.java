da0fa9c2a7e74d5ed00069b0e25010717c6c4f91
machenggong1996 <mcg1996@163.com>
2020-12-28 09:30:07 +0800

    买股票IV

--- src/leetcode/dp/MaxProfit.java
+++ src/leetcode/dp/MaxProfit.java
@@ -1,5 +1,7 @@
 package leetcode.dp;
 
+import java.util.Arrays;
+
 /**
  * @author machenggong
  * @date 2020/12/17
@@ -49,8 +51,43 @@ public class MaxProfit {
         return dp[n - 1][0];
     }
 
+    /**
+     * 买卖股票的最佳时机 IV
+     *
+     * @param k      允许交易的次数
+     * @param prices 股票价格
+     * @return
+     */
+    public static int maxProfit(int k, int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        k = Math.min(k, n / 2);
+        int[][] buy = new int[n][k + 1];
+        int[][] sell = new int[n][k + 1];
+
+        buy[0][0] = -prices[0];
+        sell[0][0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            buy[0][i] = sell[0][i] = Integer.MIN_VALUE / 2;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            buy[i][0] = Math.max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+            for (int j = 1; j <= k; ++j) {
+                buy[i][j] = Math.max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+                sell[i][j] = Math.max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
+            }
+        }
+
+        return Arrays.stream(sell[n - 1]).max().getAsInt();
+    }
+
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        //System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        System.out.println(maxProfit(2,new int[]{1, 3, 2, 8, 4, 9}));
     }
 
 
