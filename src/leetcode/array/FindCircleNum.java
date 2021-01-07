82851a3badd34df47bbed53c666fec1768fed677
machenggong1996 <mcg1996@163.com>
2021-01-07 09:26:15 +0800

    省份数量

---
+++ src/leetcode/array/FindCircleNum.java
@@ -0,0 +1,77 @@
+package leetcode.array;
+
+/**
+ * @author machenggong
+ * @date 2021/1/7
+ * @description
+ */
+public class FindCircleNum {
+
+    /**
+     * 547. 省份数量
+     * 直接相邻的叫城市 属于同一省份 不直接相邻的叫省份
+     *
+     * @param isConnected 矩阵
+     * @return 数量
+     */
+    public static int findCircleNum(int[][] isConnected) {
+        int provinces = isConnected.length;
+        boolean[] visited = new boolean[provinces];
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            if (!visited[i]) {
+                dfs(isConnected, visited, provinces, i);
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public static void dfs(int[][] isConnected, boolean[] visited, int provinces, int i) {
+        for (int j = 0; j < provinces; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(isConnected, visited, provinces, j);
+            }
+        }
+    }
+
+    public static int findCircleNum1(int[][] isConnected) {
+        int provinces = isConnected.length;
+        int[] parent = new int[provinces];
+        for (int i = 0; i < provinces; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < provinces; i++) {
+            for (int j = i + 1; j < provinces; j++) {
+                if (isConnected[i][j] == 1) {
+                    union(parent, i, j);
+                }
+            }
+        }
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            if (parent[i] == i) {
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public static void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public static int find(int[] parent, int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent, parent[index]);
+        }
+        return parent[index];
+    }
+
+    public static void main(String[] args) {
+        int[][] isConnected = new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        System.out.println(findCircleNum1(isConnected));
+    }
+
+}
