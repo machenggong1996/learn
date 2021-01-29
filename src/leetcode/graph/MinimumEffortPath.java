4434634566e8bafbc0adfba97a614306ba7af69f
machenggong1996 <mcg1996@163.com>
2021-01-29 09:40:10 +0800

    1631. 最小体力消耗路径

---
+++ src/leetcode/graph/MinimumEffortPath.java
@@ -0,0 +1,93 @@
+package leetcode.graph;
+
+import java.util.*;
+
+/**
+ * @author machenggong
+ * @date 2021/1/29
+ * @description
+ */
+public class MinimumEffortPath {
+
+    /**
+     * 1631. 最小体力消耗路径
+     * @param heights
+     * @return
+     */
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        List<int[]> edges = new ArrayList<int[]>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int id = i * n + j;
+                if (i > 0) {
+                    edges.add(new int[]{id - n, id, Math.abs(heights[i][j] - heights[i - 1][j])});
+                }
+                if (j > 0) {
+                    edges.add(new int[]{id - 1, id, Math.abs(heights[i][j] - heights[i][j - 1])});
+                }
+            }
+        }
+        edges.sort(Comparator.comparingInt(edge -> edge[2]));
+
+        UnionFindMin uf = new UnionFindMin(m * n);
+        int ans = 0;
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1], v = edge[2];
+            uf.unite(x, y);
+            if (uf.connected(0, m * n - 1)) {
+                ans = v;
+                break;
+            }
+        }
+        return ans;
+    }
+    // 并查集模
+}
+
+class UnionFindMin {
+    int[] parent;
+    int[] size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+
+    public UnionFindMin(int n) {
+        this.n = n;
+        this.setCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    public int findset(int x) {
+        return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+    }
+
+    public boolean unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+
+    public boolean connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+}
