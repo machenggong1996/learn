c04796bed4dbcebaf676ed18d389106f213ecccd
machenggong1996 <mcg1996@163.com>
2021-02-04 17:49:25 +0800

    增加注释

--- src/leetcode/list/OddEvenList.java
+++ src/leetcode/list/OddEvenList.java
@@ -7,7 +7,9 @@ package leetcode.list;
 public class OddEvenList {
 
     /**
-     * 奇偶链表
+     * 328 奇偶链表
+     * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+     *
      * @param head
      * @return
      */
