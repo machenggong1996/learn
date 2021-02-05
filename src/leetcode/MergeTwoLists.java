1299b5612bb7d9912de547295abca24f7f4de315
machenggong1996 <mcg1996@163.com>
2021-02-05 17:14:44 +0800

    链表合并

--- src/leetcode/MergeTwoLists.java
+++ src/leetcode/MergeTwoLists.java
@@ -40,6 +40,23 @@ public class MergeTwoLists {
         return resNode.next;
     }
 
+    public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode dum = new ListNode(0), cur = dum;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dum.next;
+    }
+
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(1);
         ListNode listNode2 = new ListNode(2);
@@ -58,7 +75,7 @@ public class MergeTwoLists {
         listNode6.next = listNode7;
         listNode7.next = listNode8;
 
-       ListNode node = mergeTwoLists(listNode1, listNode4);
+       ListNode node = mergeTwoLists1(listNode1, listNode4);
        System.out.println();
     }
 
