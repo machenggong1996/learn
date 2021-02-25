a42041656f683df64890cd6c6e7cb3124d5be852
machenggong1996 <mcg1996@163.com>
2021-02-25 20:54:12 +0800

    常量池

---
+++ src/bishi/MethodAreaTest.java
@@ -0,0 +1,26 @@
+package bishi;
+
+/**
+ * @author machenggong
+ * @date 2021/2/25
+ * @description
+ */
+public class MethodAreaTest {
+
+    public static void main(String[] args) {
+        Order order = null;
+        order.hello();
+        System.out.println(order.count);
+    }
+
+}
+
+class Order{
+
+    public static int count = 1;
+
+    public static void hello(){
+        System.out.println("hello");
+    }
+
+}
