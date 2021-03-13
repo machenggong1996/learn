845a0adda355b4dd7d987fddac838bd010679072
machenggong1996 <mcg1996@163.com>
2021-03-13 08:47:09 +0800

    更新文档

---
+++ doc/java/java基础面试题.md
@@ -0,0 +1,12 @@
+# java基础面试题
+
+## java创建对象的方式
+
+* 使用new关键字
+* 使用Class类的newInstance方法
+* 使用Constructor类的newInstance方法
+* 使用clone方法
+* 使用反序列化
+* unsafe
+
+## 
--- src/leetcode/array/SortArrayByParityII.java
+++ src/leetcode/array/SortArrayByParityII.java
@@ -29,6 +29,54 @@ public class SortArrayByParityII {
         return A;
     }
 
+    /**
+     * 不改变相对顺序
+     *
+     * @param array
+     */
+    public static void reOrderArray(int[] array) {
+        /**
+         * 算法思路：
+         * 采用冒泡法思想，只要遇到前偶后奇则交换
+         */
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] % 2 == 0 && array[j + 1] % 2 == 1) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 不改变相对顺序
+     *
+     * @param array
+     * @return
+     */
+    public static int[] reOrder(int[] array) {
+        int[] resArr = new int[array.length];
+        int index = 0;
+        for (int k : array) {
+            if (k % 2 == 1) {
+                resArr[index] = k;
+                index++;
+            }
+        }
+
+        for (int j : array) {
+            if (j % 2 == 0) {
+                resArr[index] = j;
+                index++;
+            }
+        }
+
+        return resArr;
+    }
+
     public static void swap(int[] A, int i, int j) {
         int temp = A[i];
         A[i] = A[j];
@@ -36,7 +84,13 @@ public class SortArrayByParityII {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sortArrayByParityII(new int[] { 4, 2, 5, 7, 9, 8 })));
+        System.out.println(Arrays.toString(sortArrayByParityII(new int[]{4, 2, 5, 7, 9, 8})));
+        int[] arr = new int[]{4, 2, 5, 7, 9, 8};
+        reOrderArray(arr);
+        System.out.println("不改变相对顺序");
+        System.out.println(Arrays.toString(arr));
+        int[] arr1 = new int[]{4, 2, 5, 7, 9, 8};
+        System.out.println(Arrays.toString(reOrder(arr1)));
     }
 
 }
