7b0d707f227def750b0f2dda0546f3546cd4cc64
machenggong1996 <mcg1996@163.com>
2021-03-18 09:07:33 +0800

    更新文档

--- src/leetcode/array/SpiralOrder.java
+++ src/leetcode/array/SpiralOrder.java
@@ -20,6 +20,7 @@ public class SpiralOrder {
         if (matrix.length == 0) {
             return new int[0];
         }
+        // l 左边，r 右边，t 上面，b 下面，x 结果数组的位置
         int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
         int[] res = new int[(r + 1) * (b + 1)];
         while (true) {
@@ -81,10 +82,51 @@ public class SpiralOrder {
         return order;
     }
 
+    public static int[] spiralOrder2(int[][] matrix) {
+        if (matrix.length == 0) {
+            return new int[0];
+        }
+        int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
+        int[] res = new int[(r + 1) * (b + 1)];
+        while (true) {
+            // left to right.
+            for (int i = l; i <= r; i++) {
+                res[x++] = matrix[t][i];
+            }
+            if (++t > b) {
+                break;
+            }
+            // top to bottom.
+            for (int i = t; i <= b; i++) {
+                res[x++] = matrix[i][r];
+            }
+            if (l > --r) {
+                break;
+            }
+            // right to left.
+            for (int i = r; i >= l; i--) {
+                res[x++] = matrix[b][i];
+            }
+            if (t > --b) {
+                break;
+            }
+            // bottom to top.
+            for (int i = b; i >= t; i--) {
+                res[x++] = matrix[i][l];
+            }
+            if (++l > r) {
+                break;
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
-        int[][] arr = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] arr = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         System.out.println(Arrays.toString(spiralOrder(arr)));
         System.out.println(spiralOrder1(arr));
+        System.out.println(Arrays.toString(spiralOrder2(arr)));
     }
 
 }
