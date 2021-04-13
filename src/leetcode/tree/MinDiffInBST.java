03cd39ed596009e67032e4985d836ce10b78712e
马成功 <machenggong@machenggongdeMacBook-Pro.local>
2021-04-13 08:59:57 +0800

    二叉树

---
+++ src/leetcode/tree/MinDiffInBST.java
@@ -0,0 +1,48 @@
+package leetcode.tree;
+
+/**
+ * @author machenggong
+ * @since 2021/4/13
+ */
+public class MinDiffInBST {
+
+    int pre;
+    int ans;
+
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre == -1) {
+            pre = root.val;
+        } else {
+            ans = Math.min(ans, root.val - pre);
+            pre = root.val;
+        }
+        dfs(root.right);
+    }
+
+    public static void main(String[] args) {
+        MinDiffInBST bst = new MinDiffInBST();
+        TreeNode root = new TreeNode(4);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(6);
+        TreeNode node3 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(3);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+
+        System.out.println(bst.minDiffInBST(root));
+    }
+
+}
