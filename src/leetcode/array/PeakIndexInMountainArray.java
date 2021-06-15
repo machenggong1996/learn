606bc3be24a520395a1a5a0b7e3137022fe1de76
马成功 <machenggong@machenggongdeMacBook-Pro.local>
2021-06-15 08:58:37 +0800

    852. 山脉数组的峰顶索引

--- src/leetcode/array/PeakIndexInMountainArray.java
+++ src/leetcode/array/PeakIndexInMountainArray.java
@@ -1,6 +1,35 @@
-package leetcode.array;/**
- *
+package leetcode.array;
+
+/**
  * @author machenggong
  * @since 2021/6/15
- */public class PeakIndexInMountainArray {
+ */
+public class PeakIndexInMountainArray {
+
+    /**
+     * 852. 山脉数组的峰顶索引 二分查找
+     *
+     * @param arr
+     * @return
+     */
+    public static int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+        int left = 1, right = n - 2, ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 10, 5, 2};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
 }
