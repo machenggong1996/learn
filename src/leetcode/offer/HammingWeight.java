461d5976d4f51a592f7c736f20ef3acb13fb7496
马成功 <machenggong@machenggongdeMacBook-Pro.local>
2021-06-23 08:46:56 +0800

    二进制中1的个数

--- src/leetcode/offer/HammingWeight.java
+++ src/leetcode/offer/HammingWeight.java
@@ -5,6 +5,11 @@ package leetcode.offer;
  */
 public class HammingWeight {
 
+    /**
+     * 剑指 Offer 15. 二进制中1的个数
+     * @param n
+     * @return
+     */
     // you need to treat n as an unsigned value
     public static int hammingWeight(int n) {
 
@@ -18,8 +23,17 @@ public class HammingWeight {
         return count;
     }
 
+    public static int hammingWeight1(int n) {
+        int ret = 0;
+        while (n != 0) {
+            n &= n - 1;
+            ret++;
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
-        System.out.println(hammingWeight(0b11111111111111111111111111111101));
+        System.out.println(hammingWeight1(0b11111111111111111111111111111101));
     }
 
 }
