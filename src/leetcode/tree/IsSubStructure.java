ca2afeffbc3da51704972c65b7567d963c5f0a38
machenggong1996 <mcg1996@163.com>
2021-07-23 09:33:32 +0800

    树的子结构

--- src/leetcode/tree/IsSubStructure.java
+++ src/leetcode/tree/IsSubStructure.java
@@ -8,7 +8,8 @@ package leetcode.tree;
 public class IsSubStructure {
 
     /**
-     * 树的子结构
+     * 剑指 Offer 26. 树的子结构
+     *
      * @param A
      * @param B
      * @return
@@ -17,6 +18,15 @@ public class IsSubStructure {
         return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
     }
 
+    /**
+     * 判断树 A 中 以 n_An
+     * A
+     * 为根节点的子树 是否包含树 B
+     *
+     * @param A
+     * @param B
+     * @return
+     */
     private static boolean recur(TreeNode A, TreeNode B) {
         if (B == null) {
             return true;
@@ -25,6 +35,35 @@ public class IsSubStructure {
             return false;
         }
         return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+
+    /**
+     * 3
+     * / \
+     * 4   5
+     * / \
+     * 1   2
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+
+        node3.left = node4;
+        node3.right = node5;
+
+        node4.left = node1;
+        node4.right = node2;
+
+        TreeNode subNode4 = new TreeNode(4);
+
+        subNode4.left = new TreeNode(1);
+
+        System.out.println(isSubStructure(node3, subNode4));
     }
 
 
