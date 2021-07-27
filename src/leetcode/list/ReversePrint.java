35fa46124349390533f8410fba09f73296bd15bf
machenggong1996 <mcg1996@163.com>
2021-07-27 09:18:16 +0800

    从尾到头打印链表

---
+++ src/leetcode/list/ReversePrint.java
@@ -0,0 +1,61 @@
+package leetcode.list;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * @author machenggong
+ * @since 2021/7/27
+ */
+public class ReversePrint {
+
+    /**
+     * 面试题06. 从尾到头打印链表
+     *
+     * @param head
+     * @return
+     */
+    public static int[] reversePrint(ListNode head) {
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        int size = stack.size();
+        int[] print = new int[size];
+        for (int i = 0; i < size; i++) {
+            print[i] = stack.pop().val;
+        }
+        return print;
+    }
+
+    public static int[] reversePrint1(ListNode head) {
+        ListNode node = head;
+        int count = 0;
+        while (node != null) {
+            ++count;
+            node = node.next;
+        }
+        int[] nums = new int[count];
+        node = head;
+        // 倒着进数组
+        for (int i = count - 1; i >= 0; --i) {
+            nums[i] = node.val;
+            node = node.next;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+
+        node1.next = node2;
+        node2.next = node3;
+
+        System.out.println(Arrays.toString(reversePrint1(node1)));
+    }
+
+}
