35682d28ba568f82b52863c51de88c397c1ff362
machenggong1996 <mcg1996@163.com>
2022-03-24 09:38:57 +0800

    二叉搜索树的后序遍历序列

--- src/leetcode/offer/VerifyPostorder.java
+++ src/leetcode/offer/VerifyPostorder.java
@@ -19,7 +19,16 @@ public class VerifyPostorder {
         return recur(postorder, 0, postorder.length - 1);
     }
 
+    /**
+     * {1,2,5,10,6,9,4,3} 应该遵循 0到1位都比最后一位小 2到6位都比最后一位大 通过这种规则区分出每个子树 继而递归 发现 {5,10,6,9} 这棵子树不满足这个规则 6小于9 这也是下面p==j的含义
+     *
+     * @param postorder
+     * @param i
+     * @param j
+     * @return
+     */
     static boolean recur(int[] postorder, int i, int j) {
+        // 说明就一个节点
         if (i >= j) {
             return true;
         }
@@ -32,7 +41,7 @@ public class VerifyPostorder {
         while (postorder[p] > postorder[j]) {
             p++;
         }
-        // p == j 判断此树是否正确
+        // p == j 判断此树是否正确  i到m-1是左子树 m到j-1是右子树 j是根
         return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
     }
 
@@ -58,8 +67,10 @@ public class VerifyPostorder {
     }
 
     public static void main(String[] args) {
-        int[] postorder = new int[]{1, 3, 2, 6, 5};
-        System.out.println(verifyPostorder(postorder));
+        int[] postorderTrue = new int[]{1, 3, 2, 6, 5};
+        int[] postorderFalse = {1, 2, 5, 10, 6, 9, 4, 3};
+        System.out.println(verifyPostorder(postorderFalse));
+        System.out.println(verifyPostorder1(postorderTrue));
     }
 
 
