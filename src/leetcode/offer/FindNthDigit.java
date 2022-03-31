e930280c8ca70bed2a17a392f4a41ce41b3a3601
machenggong1996 <mcg1996@163.com>
2022-03-31 09:15:45 +0800

    数字序列中某一位的数字

---
+++ src/leetcode/offer/FindNthDigit.java
@@ -0,0 +1,34 @@
+package leetcode.offer;
+
+/**
+ * @author machenggong
+ * @since 2022/3/31
+ */
+public class FindNthDigit {
+
+    /**
+     * 剑指 Offer 44. 数字序列中某一位的数字
+     *
+     * @param n
+     * @return
+     */
+    public static int findNthDigit(int n) {
+        int digit = 1;
+        long start = 1;
+        // 数位数量
+        long count = 9;
+        while (n > count) { // 1.
+            n -= count;
+            digit += 1;
+            start *= 10;
+            count = digit * start * 9;
+        }
+        long num = start + (n - 1) / digit; // 2.
+        return Long.toString(num).charAt((n - 1) % digit) - '0'; // 3.
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findNthDigit(11));
+    }
+
+}
