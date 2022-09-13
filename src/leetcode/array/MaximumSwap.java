80f51a16fd118636841a601083a5f2bf27e236ce
machenggong1996 <mcg1996@163.com>
2022-09-13 10:19:05 +0800

    更新算法

---
+++ src/leetcode/array/MaximumSwap.java
@@ -0,0 +1,33 @@
+package leetcode.array;
+
+/**
+ * @author machenggong
+ * @since 2022/9/13
+ */
+public class MaximumSwap {
+
+    public static int maximumSwap(int num) {
+        char[] charArray = String.valueOf(num).toCharArray();
+        int n = charArray.length;
+        int maxNum = num;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(charArray, i, j);
+                maxNum = Math.max(maxNum, Integer.parseInt(new String(charArray)));
+                swap(charArray, i, j);
+            }
+        }
+        return maxNum;
+    }
+
+    public static void swap(char[] charArray, int i, int j) {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximumSwap(2736));
+    }
+
+}
